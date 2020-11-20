fb312bdd7325c4f1a844bf39936bd411a71772c0
Daniel Asante <baremetals16@gmail.com>
2020-11-20 19:01:15 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Products from './Products';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  formatName(user){ return user.firstName + ' ' + user.lastName; }
+
+  render() {
+
+    const user ={ firstName:'Greg', lastName:'Lim' };
+
+    return (
+      <div>
+        <h1>
+          Hello, {this.formatName(user)}
+          <Products />
+        </h1>
+      </div>
+    )
+  }
 }
 
 export default App;
